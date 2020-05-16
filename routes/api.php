177b419388190be11a794d02afb7b3175cab85ec
kade-d <44915998+kade-d@users.noreply.github.com>
2020-05-15 23:25:00 -0400

    Ran `laravel new BusShuttleAPI --force` to fix missing file errors. Had to replace contents of some files.

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,24 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-$name = Route::currentRouteName();
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
 
-Route::pattern('id', '[0-9]+');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::pattern("id", "[0-9]+");
 
 Route::get('buses', 'BusController@get');
 Route::get('buses/{id}', 'BusController@find');
@@ -28,7 +42,7 @@ Route::delete('stops/{id}', 'StopController@delete');
 
 Route::get('drivers', 'UserController@get');
 Route::get('drivers/{id}', 'UserController@find');
+Route::put('drivers/{id}', 'UserController@update');
 Route::post('drivers', 'UserController@create');
 Route::delete('drivers/{id}', 'UserController@delete');
-
 
